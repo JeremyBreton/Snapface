2a57fc14c2d8a336e020d9023a99359912880516
Jérémy <breton.jeremy@live.fr>
2024-09-24 15:38:48 +0200

    boucle for et upgrade angular 16>17

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -10,34 +10,40 @@ import { FaceSnap } from './models/face-snap';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  faceSnaps!: FaceSnap [];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Kobe Bryant',
-      'Kobe the Legend !',
-      'https://icibillet.com/uploads/une-rue-de-los-angeles-s-appellera-kobe-bryant_image_1.jpg',
-      new Date(),
-      24
-    );
-    this.myOtherSnap = new FaceSnap(
-      'Michael Jordan',
-      'MJ the GOAT !',
-      'https://agorasports.fr/wp-content/uploads/2020/12/Michael-Jordan.jpg',
-      new Date(),
-      23
-    );
-    this.myLastSnap = new FaceSnap(
-      'Stephen Curry',
-      'Cheeeeeeef Steeeeeeeeeeeeeph !',
-      'https://imgresizer.eurosport.com/unsafe/2560x1440/filters:format(jpeg)/origin-imgresizer.eurosport.com/2023/11/25/3831444-77861808-2560-1440.jpg',
-      new Date(),
-      30
-    );
 
-    this.myOtherSnap.setLocation('Los Angeles');
+    this.faceSnaps = [
+      new FaceSnap(
+            'Kobe Bryant',
+            'Kobe the Legend !',
+            'https://icibillet.com/uploads/une-rue-de-los-angeles-s-appellera-kobe-bryant_image_1.jpg',
+            new Date(),
+            24
+          ),
+      new FaceSnap(
+        'Michael Jordan',
+        'MJ the GOAT !',
+        'https://agorasports.fr/wp-content/uploads/2020/12/Michael-Jordan.jpg',
+        new Date(),
+        23
+      ),
+      new FaceSnap(
+        'Stephen Curry',
+        'Cheeeeeeef Steeeeeeeeeeeeeph !',
+        'https://imgresizer.eurosport.com/unsafe/2560x1440/filters:format(jpeg)/origin-imgresizer.eurosport.com/2023/11/25/3831444-77861808-2560-1440.jpg',
+        new Date(),
+        30
+      ),
+    ]
+
+    this.faceSnaps[1].setLocation('Los Angeles');
   }
 }
 
--- snapface/src/app/models/face-snap.ts
+++ snapface/src/app/models/face-snap.ts
@@ -7,18 +7,18 @@ export class FaceSnap {
                 public description: string,
                 public imageUrl: string,
                 public createdAt: Date,
-                public snaps: number, 
-                ){}
-
+                public snaps: number) {}
+  
     addSnap(): void {
-        this.snaps++;
+      this.snaps++;
     }
-
+  
     removeSnap(): void {
-        this.snaps--;
+      this.snaps--;
     }
-
+  
     setLocation(location: string): void {
-        this.location = location;
+      this.location = location;
     }
-}
+  }
+  
